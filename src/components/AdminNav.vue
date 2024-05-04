12ee063db44e24f387a009a36ec9a8002a182a17
fansuregrin <pwz113436@gmail.com>
2024-05-04 15:57:26 +0800

    Feat: Generate menu options based on user permissions

--- src/components/AdminNav.vue
+++ src/components/AdminNav.vue
@@ -7,6 +7,7 @@
 import { h } from 'vue';
 import { NMenu, NIcon, createDiscreteApi } from 'naive-ui';
 import { RouterLink } from 'vue-router';
+import axios from 'axios';
 
 const { message } = createDiscreteApi(['message']);
 
@@ -14,65 +15,30 @@ function renderIcon(icon) {
   return () => h(NIcon, null, { default: () => h(icon) });
 }
 
-const menuOptions = [
-  {
-    label: '控制台',
-    key: 'console',
-    children: [
-      {
-        label: () => h(
-          RouterLink,
-          {
-            to: {
-              name: "UserCenter",
-            }
-          },
-          { default: () => '个人中心'}
-        ),
-        key: 'user-center'
-      }
-    ]
-  },
-  {
-    label: '管理',
-    key: 'management',
-    children: [
-      {
-        label: () => h(
-          RouterLink,
-          {
-            to: { name: 'ManageArticle'}
-          },
-          { default: () => '文章' }
-        ),
-        key: 'article'
-      }
-    ]
-  },
-  {
-    label: () => h(
-      RouterLink,
-      {
-        to: {
-          path: '/'
-        }
-      },
-      { default: () => '首页' }
-    ),
-    key: 'index'
-  },
-  {
-    label: '登出',
-    key: 'logout'
+const menuItems = {
+  manage: {
+    article: {
+      label: () => h(
+        RouterLink,
+        {
+          to: { name: 'ManageArticle'}
+        },
+        { default: () => '文章' }
+      ),
+      key: 'article'
+    }
   }
-];
+};
 
 export default {
   name: 'AdminNav',
   data() {
     return {
-      menuOptions
+      menuOptions: []
     }
+  },
+  created() {
+    this.genMenuOptions();
   },
   methods: {
     logout() {
@@ -84,6 +50,82 @@ export default {
       if (key === 'logout') {
         this.logout();
       }
+    },
+    genMenuOptions() {
+      const userToken = sessionStorage.getItem('userToken');
+      if (!userToken) {
+        this.$router.push({name: 'Login'});
+        return;
+      }
+      axios.get(
+        '/api/user/role',
+        {
+          headers: {
+            Authorization: `Bearer ${userToken}`
+          }
+        }
+      ).then(response => {
+        if (response.data.status === 0) {
+          const role = response.data.role;
+          console.log(role);
+          this.menuOptions = [];
+          this.menuOptions.push(
+            {
+              label: '控制台',
+              key: 'console',
+              children: [
+                {
+                  label: () => h(
+                    RouterLink,
+                    {
+                      to: {
+                        name: "UserCenter",
+                      }
+                    },
+                    { default: () => '个人中心'}
+                  ),
+                  key: 'user-center'
+                }
+              ]
+            }
+          );
+          // "管理"菜单选项
+          if (role.manage) {
+            this.menuOptions.push({
+              label: '管理',
+              key: 'management',
+              children: []
+            });
+            for (const name of role.manage) {
+              this.menuOptions[1].children.push(menuItems.manage[name]);
+            }
+          }
+          // "首页"菜单选项
+          this.menuOptions.push(
+            {
+              label: () => h(
+                RouterLink,
+                {
+                  to: {
+                    path: '/'
+                  }
+                },
+                { default: () => '首页' }
+              ),
+              key: 'index'
+            }
+          );
+          // "登出"菜单选项
+          this.menuOptions.push(
+            {
+              label: '登出',
+              key: 'logout'
+            }
+          );
+        }
+      }).catch(error => {
+        console.log(error.message);
+      })
     }
   }
 }
