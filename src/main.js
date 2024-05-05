f6729dd737176365cb3125acfc73b3b388e91ee2
fansuregrin <pwz113436@gmail.com>
2024-05-05 15:17:03 +0800

    introduce vue-i18n into libre-blog

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 import App from './App.vue'
 import naive from 'naive-ui'
 import router from './router'
 
+const i18n = createI18n();
 const app = createApp(App)
 
 app.use(router)
 app.use(naive)
+app.use(i18n)
 app.mount('#app')
